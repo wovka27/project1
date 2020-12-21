443e3a001af8a0cf074fc7886409d49e1f978a57
Vladimir <70747356+wovka27@users.noreply.github.com>
2020-12-21 23:43:55 +0700

    portfolio

--- js/script.js
+++ js/script.js
@@ -1,34 +1,84 @@
+let load = document.querySelector('.onload');
+window.onload = () => load.classList.add('off');
+function addCls(item,cls){item.classList.add(cls)}
+function rmvCls(item,cls){item.classList.remove(cls)}
+function tglCls(item,cls){item.classList.toggle(cls)}
+function cntCls(item,cls){item.classList.contains(cls)}
+function eventFun(item, event, handler) {item.addEventListener(event, handler)}
 let menu        = document.querySelector('.menu');
 let burger      = menu.querySelector('.menu__burger');
 let mBody       = menu.querySelector('.menu__body');
 let mItems      = menu.querySelectorAll('li');
+let wWidth      = window.innerWidth;
 let posY        = 0;
+let about       = document.querySelector('.about');
+setTimeout(()=>{menu.classList.add('show')}, 1000)
+function menuOpen() {
+    tglCls(mBody,'active');
+    tglCls(burger,'active');
+    if(wWidth <768) tglCls(burger, 'touch');
+}
+
 function menuClose() {
-    mBody.classList.remove('active');
-    burger.classList.remove('active');
+    rmvCls(mBody,'active');
+    rmvCls(burger,'active');
+    if(wWidth <768) rmvCls(burger, 'touch');
 }
-function menuToggle() {
-    mBody.classList.toggle('active');
-    burger.classList.toggle('active');
-}
-burger.addEventListener('click', function () {
-        if (window.innerWidth > 768) { menuToggle(); }
-        else if (window.innerWidth <= 768) { menuToggle(); }
-    })
-function menuhide() {
-    window.addEventListener('wheel', e =>{
-        if(e.wheelDelta == '120') {menu.classList.add('show');}
-        else {menu.classList.remove('show');}
-        if (mBody.classList.contains('active')||burger.classList.contains('active')) {menuClose();}
-    })
-}
-mItems.forEach(el => {el.addEventListener('click', e =>{menuClose()})});
 
-
-window.addEventListener('touchmove', e =>{
+function adaptOpen() {
+    if (wWidth > 768) menuOpen();
+    else if (wWidth <= 768) menuOpen();
+}
+function wheelOutMenu(item) {
+    if(item.wheelDelta == '120') {addCls(menu,'show');}
+    else  {rmvCls(menu,'show');}
+    if (mBody.classList.contains('active')) menuClose();
+}
+function touchOutMenu(e) {
     const {clientX, clientY} = e.touches[0];
-    if (posY < clientY) {menu.classList.add('show');console.log('вниз');}
-    if (posY > clientY) {menu.classList.remove('show');console.log('вверх');}
-    if (mBody.classList.contains('active')||burger.classList.contains('active')) {menuClose();}
+    if (posY < clientY) addCls(menu, 'show');
+    if (posY > clientY) rmvCls(menu, 'show');
+    if (mBody.classList.contains('active')) menuClose();
     posY = clientY;
-})
+}
+eventFun(burger, 'click', adaptOpen);
+eventFun(window, 'wheel', wheelOutMenu);
+eventFun(window, 'touchmove', touchOutMenu);
+mItems.forEach(el => {
+    eventFun(el, 'click' ,menuClose);
+    el.addEventListener('click', e => {
+        if (e.target.innerHTML == 'Контакты') contact.classList.add('active');
+        if (e.target.innerHTML == 'О Сайте') about.classList.add('active');
+    })
+});;
+const container = document.querySelector('.bg-img');
+const track = container.querySelector('ul');
+let slides = container.querySelectorAll('ul li');
+let index = 0;
+let slideInterval = setInterval(nextSlide,5000);
+function nextSlide() {
+    slides[index].className = 'bg-img__link';
+    index = (index+1)%slides.length;
+    slides[index].className = 'bg-img__link show';
+}
+;
+
+let contact = document.querySelector('.contact');
+const close = document.getElementById('close');
+const input = contact.querySelectorAll('input');
+const submit = contact.querySelector('button');
+function closeForm() {contact.classList.remove('active');}
+close.onclick = closeForm;
+submit.onclick = closeForm;
+
+const aboutClose = document.querySelector('#aboutClose');
+aboutClose.onclick = () => about.classList.remove('active');
+
+const blurBtn = document.querySelector('.blur-btn');
+blurBtn.addEventListener('click', e => {
+    container.classList.toggle('not-blur');
+    if (blurBtn.innerHTML == 'off') {
+        blurBtn.textContent = 'on'
+    }else blurBtn.textContent = 'off'
+});
+
